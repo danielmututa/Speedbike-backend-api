2284170c69550b5a2ca6a45326611cd3e8b1b5b7
unknown <mututadaniel54@gmail.com>
2024-11-23 11:29:21 -0800

    updating bikes

--- src/routes/bikeRoutes.js
+++ src/routes/bikeRoutes.js
@@ -20,7 +20,7 @@ const upload = multer({
 router.get('/search', bikeController.searchBikes);
 
 // GET all bikes available for purchase
-router.get('/available', bikeController.getBikesForPurchase);
+router.get('/available',upload.single('image'), bikeController.getBikesForPurchase);
 
 // GET all bikes for a specific user
 router.get('/user/:userId', bikeController.getBikes);
